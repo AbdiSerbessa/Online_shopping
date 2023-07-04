1ce2ee7da7a1ca3cab5880b44b2c790a0cfe83b8
Muhal10 <129385993+Muhal10@users.noreply.github.com>
2023-07-04 12:40:40 +0300

    js commit all

--- main.js
+++ main.js
@@ -1,336 +1,174 @@
-$(document).ready(function(){
-	cat();
-	brand();
-	product();
-	//cat() is a funtion fetching category record from database whenever page is load
-	function cat(){
-		$.ajax({
-			url	:	"action.php",
-			method:	"POST",
-			data	:	{category:1},
-			success	:	function(data){
-				$("#get_category").html(data);
+(function($) {
+	"use strict"
 
-			}
-		})
-	}
-	//brand() is a funtion fetching brand record from database whenever page is load
-	function brand(){
-		$.ajax({
-			url	:	"action.php",
-			method:	"POST",
-			data	:	{brand:1},
-			success	:	function(data){
-				$("#get_brand").html(data);
-			}
-		})
-	}
-	//product() is a funtion fetching product record from database whenever page is load
-		function product(){
-		$.ajax({
-			url	:	"action.php",
-			method:	"POST",
-			data	:	{getProduct:1},
-			success	:	function(data){
-				$("#get_product").html(data);
-			}
-		})
-	}
-	/*	when page is load successfully then there is a list of categories when user click on category we will get category id and
-		according to id we will show products
-	*/
-	$("body").delegate(".category","click",function(event){
-		$("#get_product").html("<h3>Loading...</h3>");
-		event.preventDefault();
-		var cid = $(this).attr('cid');
-
-			$.ajax({
-			url		:	"action.php",
-			method	:	"POST",
-			data	:	{get_seleted_Category:1,cat_id:cid},
-			success	:	function(data){
-				$("#get_product").html(data);
-				if($("body").width() < 480){
-					$("body").scrollTop(683);
-				}
-			}
-		})
-
+	// Mobile Nav toggle
+	$('.menu-toggle > a').on('click', function (e) {
+		e.preventDefault();
+		$('#responsive-nav').toggleClass('active');
 	})
 
-	/*	when page is load successfully then there is a list of brands when user click on brand we will get brand id and
-		according to brand id we will show products
-	*/
-	$("body").delegate(".selectBrand","click",function(event){
-		event.preventDefault();
-		$("#get_product").html("<h3>Loading...</h3>");
-		var bid = $(this).attr('bid');
+	// Fix cart dropdown from closing
+	$('.cart-dropdown').on('click', function (e) {
+		e.stopPropagation();
+	});
+	
+	
+	/////////////////////////////////////////
+	
 
-			$.ajax({
-			url		:	"action.php",
-			method	:	"POST",
-			data	:	{selectBrand:1,brand_id:bid},
-			success	:	function(data){
-				$("#get_product").html(data);
-				if($("body").width() < 480){
-					$("body").scrollTop(683);
-				}
-			}
+	// Products Slick
+	$('.products-slick').each(function() {
+		var $this = $(this),
+				$nav = $this.attr('data-nav');
+
+		$this.slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			autoplay: true,
+			infinite: true,
+			speed: 300,
+			dots: false,
+			arrows: true,
+			appendArrows: $nav ? $nav : false,
+			responsive: [{
+	        breakpoint: 991,
+	        settings: {
+	          slidesToShow: 2,
+	          slidesToScroll: 1,
+	        }
+	      },
+	      {
+	        breakpoint: 480,
+	        settings: {
+	          slidesToShow: 1,
+	          slidesToScroll: 1,
+	        }
+	      },
+	    ]
+		});
+	});
+
+	// Products Widget Slick
+	$('.products-widget-slick').each(function() {
+		var $this = $(this),
+				$nav = $this.attr('data-nav');
+
+		$this.slick({
+			infinite: true,
+			autoplay: true,
+			speed: 300,
+			dots: false,
+			arrows: true,
+			appendArrows: $nav ? $nav : false,
+		});
+	});
+
+	/////////////////////////////////////////
+
+	// Product Main img Slick
+	$('#product-main-img').slick({
+    infinite: true,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    fade: true,
+    asNavFor: '#product-imgs',
+  });
+
+	// Product imgs Slick
+  $('#product-imgs').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+		centerPadding: 0,
+		vertical: true,
+    asNavFor: '#product-main-img',
+		responsive: [{
+        breakpoint: 991,
+        settings: {
+					vertical: false,
+					arrows: false,
+					dots: true,
+        }
+      },
+    ]
+  });
+
+	// Product img zoom
+	var zoomMainProduct = document.getElementById('product-main-img');
+	if (zoomMainProduct) {
+		$('#product-main-img .product-preview').zoom();
+	}
+
+
+	
+	/////////////////////////////////////////
+	
+	// Input number
+	$('.input-number').each(function() {
+		var $this = $(this),
+		$input = $this.find('input[type="number"]'),
+		up = $this.find('.qty-up'),
+		down = $this.find('.qty-down');
+
+		down.on('click', function () {
+			var value = parseInt($input.val()) - 1;
+			value = value < 1 ? 1 : value;
+			$input.val(value);
+			$input.change();
+			updatePriceSlider($this , value)
 		})
 
-	})
-	/*
-		At the top of page there is a search box with search button when user put name of product then we will take the user
-		given string and with the help of sql query we will match user given string to our database keywords column then matched product
-		we will show
-	*/
-	$("#search_btn").click(function(){
-		$("#get_product").html("<h3>Loading...</h3>");
-		var keyword = $("#search").val();
-		if(keyword != ""){
-			$.ajax({
-			url		:	"action.php",
-			method	:	"POST",
-			data	:	{search:1,keyword:keyword},
-			success	:	function(data){
-				$("#get_product").html(data);
-				if($("body").width() < 480){
-					$("body").scrollTop(683);
-				}
-			}
+		up.on('click', function () {
+			var value = parseInt($input.val()) + 1;
+			$input.val(value);
+			$input.change();
+			updatePriceSlider($this , value)
 		})
+	});
+
+	var priceInputMax = document.getElementById('price-max'),
+			priceInputMin = document.getElementById('price-min');
+
+	priceInputMax.addEventListener('change', function(){
+		updatePriceSlider($(this).parent() , this.value)
+	});
+
+	priceInputMin.addEventListener('change', function(){
+		updatePriceSlider($(this).parent() , this.value)
+	});
+
+	function updatePriceSlider(elem , value) {
+		if ( elem.hasClass('price-min') ) {
+			console.log('min')
+			priceSlider.noUiSlider.set([value, null]);
+		} else if ( elem.hasClass('price-max')) {
+			console.log('max')
+			priceSlider.noUiSlider.set([null, value]);
 		}
-	})
-	//end
-
-
-	/*
-		Here #login is login form id and this form is available in index.php page
-		from here input data is sent to login.php page
-		if you get login_success string from login.php page means user is logged in successfully and window.location is
-		used to redirect user from home page to profile.php page
-	*/
-	$("#login").on("submit",function(event){
-		event.preventDefault();
-		$(".overlay").show();
-		$.ajax({
-			url	:	"login.php",
-			method:	"POST",
-			data	:$("#login").serialize(),
-			success	:function(data){
-				if(data == "login_success"){
-					window.location.href = "profile.php";
-				}else if(data == "cart_login"){
-					window.location.href = "cart.php";
-				}else{
-					$("#e_msg").html(data);
-					$(".overlay").hide();
-				}
-			}
-		})
-	})
-	//end
-
-	//Get User Information before checkout
-	$("#signup_form").on("submit",function(event){
-		event.preventDefault();
-		$(".overlay").show();
-		$.ajax({
-			url : "register.php",
-			method : "POST",
-			data : $("#signup_form").serialize(),
-			success : function(data){
-				$(".overlay").hide();
-				if (data == "register_success") {
-					window.location.href = "cart.php";
-				}else{
-					$("#signup_msg").html(data);
-				}
-
-			}
-		})
-	})
-	//Get User Information before checkout end here
-
-	//Add Product into Cart
-	$("body").delegate("#product","click",function(event){
-		var pid = $(this).attr("pid");
-		event.preventDefault();
-		$(".overlay").show();
-		$.ajax({
-			url : "action.php",
-			method : "POST",
-			data : {addToCart:1,proId:pid},
-			success : function(data){
-				count_item();
-				getCartItem();
-				$('#product_msg').html(data);
-				$('.overlay').hide();
-			}
-		})
-	})
-	//Add Product into Cart End Here
-	//Count user cart items funtion
-	count_item();
-	function count_item(){
-		$.ajax({
-			url : "action.php",
-			method : "POST",
-			data : {count_item:1},
-			success : function(data){
-				$(".badge").html(data);
-			}
-		})
-	}
-	//Count user cart items funtion end
-
-	//Fetch Cart item from Database to dropdown menu
-	getCartItem();
-	function getCartItem(){
-		$.ajax({
-			url : "action.php",
-			method : "POST",
-			data : {Common:1,getCartItem:1},
-			success : function(data){
-				$("#cart_product").html(data);
-			}
-		})
 	}
 
-	//Fetch Cart item from Database to dropdown menu
-
-	$("body").delegate(".qty","keyup",function(event){
-		event.preventDefault();
-		var row = $(this).parent().parent();
-		var price = row.find('.price').val();
-		var qty = row.find('.qty').val();
-		if (isNaN(qty)) {
-			qty = 1;
-		};
-		if (qty < 1) {
-			qty = 1;
-		};
-		var total = price * qty;
-		row.find('.total').val(total);
-		var net_total=0;
-		$('.total').each(function(){
-			net_total += ($(this).val()-0);
-		})
-		$('.net_total').html("Total : $ " +net_total);
-
-	})
-	//Change Quantity end here
-
-	/*
-		whenever user click on .remove class we will take product id of that row
-		and send it to action.php to perform product removal operation
-	*/
-	$("body").delegate(".remove","click",function(event){
-		var remove = $(this).parent().parent().parent();
-		var remove_id = remove.find(".remove").attr("remove_id");
-		$.ajax({
-			url	:	"action.php",
-			method	:	"POST",
-			data	:	{removeItemFromCart:1,rid:remove_id},
-			success	:	function(data){
-				$("#cart_msg").html(data);
-				checkOutDetails();
+	// Price Slider
+	var priceSlider = document.getElementById('price-slider');
+	if (priceSlider) {
+		noUiSlider.create(priceSlider, {
+			start: [1, 999],
+			connect: true,
+			step: 1,
+			range: {
+				'min': 1,
+				'max': 999
 			}
-		})
-	})
-	/*
-		whenever user click on .update class we will take product id of that row
-		and send it to action.php to perform product qty updation operation
-	*/
-	$("body").delegate(".update","click",function(event){
-		var update = $(this).parent().parent().parent();
-		var update_id = update.find(".update").attr("update_id");
-		var qty = update.find(".qty").val();
-		$.ajax({
-			url	:	"action.php",
-			method	:	"POST",
-			data	:	{updateCartItem:1,update_id:update_id,qty:qty},
-			success	:	function(data){
-				$("#cart_msg").html(data);
-				checkOutDetails();
-			}
-		})
-	})
-	checkOutDetails();
-	net_total();
+		});
 
-	function checkOutDetails(){
-	 $('.overlay').show();
-		$.ajax({
-			url : "action.php",
-			method : "POST",
-			data : {Common:1,checkOutDetails:1},
-			success : function(data){
-				$('.overlay').hide();
-				$("#cart_checkout").html(data);
-					net_total();
-			}
-		})
-	}
-	/*
-		net_total function is used to calcuate total amount of cart item
-	*/
-	function net_total(){
-		var net_total = 0;
-		$('.qty').each(function(){
-			var row = $(this).parent().parent();
-			var price  = row.find('.price').val();
-			var total = price * $(this).val()-0;
-			row.find('.total').val(total);
-		})
-		$('.total').each(function(){
-			net_total += ($(this).val()-0);
-		})
-		$('.net_total').html("Total : "+ CURRENCY+ " " +net_total);
+		priceSlider.noUiSlider.on('update', function( values, handle ) {
+			var value = values[handle];
+			handle ? priceInputMax.value = value : priceInputMin.value = value
+		});
 	}
 
-	//remove product from cart
+	
 
-	page();
-	function page(){
-		$.ajax({
-			url	:	"action.php",
-			method	:	"POST",
-			data	:	{page:1},
-			success	:	function(data){
-				$("#pageno").html(data);
-			}
-		})
-	}
-	$("body").delegate("#page","click",function(){
-		var pn = $(this).attr("page");
-		$.ajax({
-			url	:	"action.php",
-			method	:	"POST",
-			data	:	{getProduct:1,setPage:1,pageNumber:pn},
-			success	:	function(data){
-				$("#get_product").html(data);
-			}
-		})
-	})
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})(jQuery);
