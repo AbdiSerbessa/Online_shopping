8b5f11d7de68f5e89d1ea42897f07cdd87a07050
Muhal10 <129385993+Muhal10@users.noreply.github.com>
2023-07-05 08:51:23 +0300

    admin navbar com

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -1,5 +1,5 @@
  <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">My Store | Online Shopping</a>
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Store | Online Shopping</a>
   <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
   <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
@@ -22,11 +22,11 @@
     			}
 
 
-    			
+
     		}
 
     	?>
-      
+
     </li>
   </ul>
 </nav>
