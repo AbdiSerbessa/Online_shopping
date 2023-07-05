5e9a6b2f14668575c6b984ca039f10814ebc436b
Muhal10 <129385993+Muhal10@users.noreply.github.com>
2023-07-05 09:32:55 +0300

    admin customer page com

--- admin/customers.php
+++ admin/customers.php
@@ -3,43 +3,76 @@
 <?php include_once("./templates/navbar.php"); ?>
 <div class="container-fluid">
   <div class="row">
-    
+
     <?php include "./templates/sidebar.php"; ?>
 
-      <div class="row">
-      	<div class="col-10">
-      		<h2>Customers</h2>
-      	</div>
-      </div>
-      
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Mobile</th>
-              <th>Address</th>
-            </tr>
-          </thead>
-          <tbody id="customer_list">
-            <!-- <tr>
-              <td>1</td>
-              <td>ABC</td>
-              <td>FDGR.JPG</td>
-              <td>122</td>
-              <td>eLECTRONCS</td>
-              <td>aPPLE</td>
-              <td><a class="btn btn-sm btn-info"></a><a class="btn btn-sm btn-danger">Delete</a></td>
-            </tr> -->
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
-</div>
+    <?php
+include("../db.php");
+if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
+{
+$user_id=$_GET['user_id'];
 
+/*this is delet quer*/
+mysqli_query($con,"delete from user_info where user_id='$user_id'")or die("query is incorrect...");
+}
+
+?>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid">
+         <div class="col-md-14">
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">Manage User</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive ps">
+                  <table class="table tablesorter table-hover" id="">
+                    <thead class=" text-primary">
+                      <tr><th>User id</th>
+                        <th>Name</th>
+                        <th>last_name</th>
+                        <th>Email</th>
+
+                <th>User Password</th>
+                <th>mobile</th>
+                <th>city</th>
+                <th>Address</th>
+	<th><a href="addcustomer.php" class="btn btn-success">Add New</a></th>
+                    </tr></thead>
+                    <tbody>
+                      <?php
+                        $result=mysqli_query($con,"select user_id,first_name,last_name, email, password,mobile,address1,address2 from user_info")or die ("query 2 incorrect.......");
+
+                        while(list($user_id,$user_name,$user_last,$email,$user_password,$mobile,$address1,$address2)=
+                        mysqli_fetch_array($result))
+                        {
+                        echo "<tr>
+                        <td>$user_id</td>
+                         <td>$user_name</td>
+                          <td>$user_last</td>
+                           <td>$email</td>
+
+                        <td>$user_password</td>
+                         <td>$mobile</td>
+                          <td>$address1</td>
+                           <td>$address2</td>";
+                        echo"<td>
+
+                        <a class='btn btn-danger' href='customers.php?user_id=$user_id&action=delete'>Delete<div class='ripple-container'></div></a>
+                        </td></tr>";
+                        }
+                        mysqli_close($con);
+                        ?>
+                    </tbody>
+                  </table>
+                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
 
 
 <!-- Modal -->
@@ -106,7 +139,7 @@
         			<button type="button" class="btn btn-primary add-product">Add Product</button>
         		</div>
         	</div>
-        	
+
         </form>
       </div>
     </div>
