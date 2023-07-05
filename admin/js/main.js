3b6250f705bfc3c1babba9aed42e258f9dfaf22c
Muhal10 <129385993+Muhal10@users.noreply.github.com>
2023-07-05 08:48:37 +0300

    admin js main commit

--- admin/js/main.js
+++ admin/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 				var resp = $.parseJSON(response);
 				if (resp.status == 202) {
 					$("#admin-register-form").trigger("reset");
-					window.location.href = window.origin+"/onlineshopping/admin/index.php";
+					window.location.href = window.origin+"/online_shopping/admin/index.php";
 				}else if(resp.status == 303){
 					$(".message").html('<span class="text-danger">'+resp.message+'</span>');
 				}
